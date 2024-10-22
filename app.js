a40aea96f3fe9f9cc8de7afc8c18e5cf473298e9
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-10-22 21:24:42 +0700

    use socket.io for connect client + send/receive message basic

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-import { WebSocketServer } from 'ws';
 import dotenv from "dotenv";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 import AccountRouter from "./routes/AccountRouter.js";
 import CallRouter from "./routes/CallRouter.js";
-import { corsConfig, parseCookies, rateLimiter, securityHeaders } from "./security/SecurityConfig.js";
+import {
+  corsConfig,
+  parseCookies,
+  rateLimiter,
+  securityHeaders,
+} from "./security/SecurityConfig.js";
 import { connectDB } from "./utils/features.js";
 import ContactRouter from "./routes/ContactRouter.js";
 import GroupChatRouter from "./routes/GroupChatController.js";
@@ -11,13 +17,10 @@ import GroupMemberRouter from "./routes/GroupMemberRouter.js";
 import MessageRouter from "./routes/MessageRouter.js";
 import MessageStatusRouter from "./routes/MessageStatusRouter.js";
 import UserStatusRouter from "./routes/UserStatusRouter.js";
-import { socketHandler } from './websockets/SocketHandler.js';
-import UserRouter from './routes/UserRoter.js';
-
-
+import UserRouter from "./routes/UserRoter.js";
 
 dotenv.config({
-   path: "./.env",
+  path: "./.env",
 });
 
 const app = express();
@@ -29,7 +32,6 @@ app.use(rateLimiter);
 app.use(corsConfig);
 app.use(parseCookies);
 app.use(express.json());
-
 
 // API ROOT
 app.use("/api/user-status", UserStatusRouter);
@@ -46,16 +48,46 @@ app.use("/api/user", UserRouter);
 const mongoURI = process.env.MONGO_URI;
 connectDB(mongoURI);
 
-// Tạo WebSocket server
-const wss = new WebSocketServer({ port: 8081 });
+// Tạo server HTTP
+const server = http.createServer(app);
+// Thiết lập Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Thay đổi thành nguồn mà bạn muốn cho phép
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // Nếu bạn cần gửi cookie
+  },
+});
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.handshake.auth);
 
-// Xử lý sự kiện kết nối WebSocket
-wss.on('connection', (ws, req) => {
-   socketHandler(ws, req);
+  // Lắng nghe sự kiện gửi tin nhắn
+  socket.on("sendMessage", (message, next) => {
+    // Phát lại tin nhắn cho tất cả client
+    console.log("Message: " + message);
+    io.emit("receiveMessage", message);
+
+    const userId = socket.handshake.auth.userId;
+    if (!userId) {
+      return next(new Error("Authentication error"));
+    }
+
+    socket.userId = userId;
+
+    next();
+  });
+
+  socket.on("sendPrivateMessage", (data) => {
+    socket.to(data.to).emit("receivePrivateMessage", data.message);
+    console.log("Message: ", data.to, data.message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: " + socket.id);
+  });
 });
 
-
-app.listen(serverPort, () => {
-   console.log(`Server is running on port ${serverPort}`);
+server.listen(8080, () => {
+  console.log(`Server is running on port : 8080`);
 });
-
