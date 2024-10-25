3d99013148b95b677ee7ed31469fb0d7872c803f
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-10-25 22:48:05 +0700

    refactor: demo private message

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import http from "http";
-import { Server } from "socket.io";
 import AccountRouter from "./routes/AccountRouter.js";
 import CallRouter from "./routes/CallRouter.js";
 import {
@@ -18,6 +17,7 @@ import MessageRouter from "./routes/MessageRouter.js";
 import MessageStatusRouter from "./routes/MessageStatusRouter.js";
 import UserStatusRouter from "./routes/UserStatusRouter.js";
 import UserRouter from "./routes/UserRoter.js";
+import { setupSocketServer } from "./websockets/SocketHandler.js"; // Import SocketHandler
 
 dotenv.config({
   path: "./.env",
@@ -50,43 +50,9 @@ connectDB(mongoURI);
 
 // Tạo server HTTP
 const server = http.createServer(app);
+
 // Thiết lập Socket.IO
-const io = new Server(server, {
-  cors: {
-    origin: "*", // Thay đổi thành nguồn mà bạn muốn cho phép
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // Nếu bạn cần gửi cookie
-  },
-});
-io.on("connection", (socket) => {
-  console.log("A user connected: ", socket.handshake.auth);
-
-  // Lắng nghe sự kiện gửi tin nhắn
-  socket.on("sendMessage", (message, next) => {
-    // Phát lại tin nhắn cho tất cả client
-    console.log("Message: " + message);
-    io.emit("receiveMessage", message);
-
-    const userId = socket.handshake.auth.userId;
-    if (!userId) {
-      return next(new Error("Authentication error"));
-    }
-
-    socket.userId = userId;
-
-    next();
-  });
-
-  socket.on("sendPrivateMessage", (data) => {
-    socket.to(data.to).emit("receivePrivateMessage", data.message);
-    console.log("Message: ", data.to, data.message);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected: " + socket.id);
-  });
-});
+setupSocketServer(server); // Gọi hàm thiết lập socket server
 
 server.listen(8080, () => {
   console.log(`Server is running on port : 8080`);
--- websockets/SocketHandler.js
+++ websockets/SocketHandler.js
@@ -1,1 +1,59 @@
+import { Server } from "socket.io";
 
+const setupSocketServer = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  });
+
+  io.use((socket, next) => {
+    const userId = socket.handshake.auth.userId;
+    if (!userId) {
+      return next(new Error("Authentication error"));
+    }
+    socket.userId = userId;
+    next();
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected: ", socket.handshake.auth);
+
+    socket.join(socket.userId)
+
+    const users = [];
+
+    for (let [id, socket] of io.of("/").sockets) {
+      users.push({
+        userId: socket.userId, // Sử dụng userId đã gán
+        socketId: id, // Lưu lại socketId để phát tin nhắn tới người dùng đích
+      });
+    }
+
+    io.emit("getUsersOnline", users);
+
+    // Dùng broadcast để gửi sự kiện userJustConnected tới tất cả người dùng trừ người đã kết nối
+    socket.broadcast.emit("userJustConnected", {
+      userId: socket.userId,
+      socketId: socket.id,
+    });
+
+    // Nhận tin nhắn từ người gửi và gửi tin nhắn tới người nhận
+    socket.on("privateMessage", ({ message, to }) => {
+      console.log("Received message: ", message, to);
+      socket.to(to).emit("privateMessageToReceiver", {
+        message: message,
+        from: socket.userId, // Đổi từ socket.id sang socket.userId
+      });
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected: ", socket.handshake.auth);
+    });
+  });
+};
+
+export { setupSocketServer };
